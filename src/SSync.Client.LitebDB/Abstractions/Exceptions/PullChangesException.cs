77f52fc8355bd386abf8b5a3f1c9be127ee39078
Gabriel Sales <ghe_sales@hotmail.com>
2024-07-30 12:39:17 -0300

    Add Custom exceptions

---
+++ src/SSync.Client.LitebDB/Abstractions/Exceptions/PullChangesException.cs
@@ -0,0 +1,17 @@
+﻿namespace SSync.Client.LitebDB.Abstractions.Exceptions
+{
+    public class PullChangesException : Exception
+    {
+        public PullChangesException()
+        {
+        }
+
+        public PullChangesException(string? message) : base(message)
+        {
+        }
+
+        public PullChangesException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/SSync.Client.LitebDB/Abstractions/Exceptions/PushChangeException.cs
@@ -0,0 +1,17 @@
+﻿namespace SSync.Client.LitebDB.Abstractions.Exceptions
+{
+    public class PushChangeException : Exception
+    {
+        public PushChangeException()
+        {
+        }
+
+        public PushChangeException(string? message) : base(message)
+        {
+        }
+
+        public PushChangeException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
