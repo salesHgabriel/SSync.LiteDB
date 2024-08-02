9f8cdfe11af6f91a383ba4b48989a2c52148fced
Gabriel Sales <ghe_sales@hotmail.com>
2024-08-01 23:45:03 -0300

    Create more tests :)

--- test/SSync.Client.LiteDB.Tests/Sync/PullChanges_Tests.cs
+++ test/SSync.Client.LiteDB.Tests/Sync/PullChanges_Tests.cs
@@ -60,7 +60,6 @@ namespace SSync.Client.LiteDB.Tests.Sync
             Assert.Equal("Range less of zero", exception.Message);
         }
 
-        //insert
         [Fact]
         public void InsertDocumentSync_ShouldReturnCreatedAtMoreZero()
         {
@@ -75,21 +74,19 @@ namespace SSync.Client.LiteDB.Tests.Sync
             var sync = new Synchronize(database);
 
             //act
-            var collection = database.GetCollection<User>(documentName);
+            
             sync.InsertSync(new User(newUserid)
             {
                 Name = newUserName
-            }, collection);
+            }, documentName);
 
             //assert
 
-            var user = collection.FindById(newUserid);
+            var user = sync.FindByIdSync<User>(newUserid, documentName);
 
             Assert.True(user.CreatedAt > 0);
         }
 
-
-        //insert
         [Fact]
         public void InsertDocumentSync_ShouldReturnStatusCreated()
         {
@@ -104,21 +101,124 @@ namespace SSync.Client.LiteDB.Tests.Sync
             var sync = new Synchronize(database);
 
             //act
-            var collection = database.GetCollection<User>(documentName);
             sync.InsertSync(new User(newUserid)
             {
                 Name = newUserName
-            }, collection);
+            }, documentName);
 
             //assert
 
-            var user = collection.FindById(newUserid);
+            var changes = sync.PullChangesResult<User>(0, documentName, DateTime.UtcNow);
 
-            Assert.True(user.Status == StatusSync.CREATED);
+            var userCreated = changes.Changes.Created.FirstOrDefault(u => u.Id == newUserid);
+
+            Assert.True(userCreated!.Status == StatusSync.CREATED);
+        }
+
+        [Fact]
+        public void UpdateDocumentSync_ShouldReturnCreatedAtMoreZero()
+        {
+            //arrange
+            var newUserid = Guid.NewGuid();
+            var newUserName = $"Cotoso {DateTime.UtcNow.Ticks}";
+
+            var documentName = "user";
+
+            using var database = new LiteDatabase(new MemoryStream());
+
+            var sync = new Synchronize(database);
+
+            //act
+            sync.InsertSync(new User(newUserid)
+            {
+                Name = newUserName
+            }, documentName);
+
+            var user = sync.FindByIdSync<User>(newUserid, documentName);
+
+            sync.UpdateSync(user, documentName);
+
+            //assert
+
+            var changes = sync.PullChangesResult<User>(0, documentName, DateTime.UtcNow);
+
+            var userUpdated = changes.Changes.Updated.FirstOrDefault(u => u.Id == newUserid);
+
+            Assert.True(userUpdated!.UpdatedAt > 0);
+        }
+
+        [Fact]
+        public void UpdateDocumentSync_ShouldReturnStatusCreated()
+        {
+            //arrange
+            var newUserid = Guid.NewGuid();
+            var newUserName = $"Cotoso {DateTime.UtcNow.Ticks}";
+
+            var documentName = "user";
+
+            using var database = new LiteDatabase(new MemoryStream());
+
+            var sync = new Synchronize(database);
+
+            //act
+            sync.InsertSync(new User(newUserid)
+            {
+                Name = newUserName
+            }, documentName);
+
+            var user = sync.FindByIdSync<User>(newUserid, documentName);
+
+            sync.UpdateSync(user, documentName);
+
+            //assert
+
+            var changes = sync.PullChangesResult<User>(0, documentName, DateTime.UtcNow);
+
+            var userUpdated = changes.Changes.Updated.FirstOrDefault(u => u.Id == newUserid);
+
+            Assert.True(user.Status == StatusSync.UPDATED);
         }
 
 
-        //TODO: testing the update/delete operations, method was called to update the dates
+        [Fact]
+        public void InsertAndUpdatedAndDeleteRowsSync_ShouldReturnPullChangesWithSameMethods()
+        {
+       
+            var colUserName = "user";
+
+            var users = Enumerable.Range(0, 4).Select(u => new User(Guid.NewGuid())
+            {
+                Name = $"Cotoso {DateTime.UtcNow.Ticks}"
+            }).ToArray();
+
+            using var database = new LiteDatabase(new MemoryStream());
+
+            var sync = new Synchronize(database);
+
+            foreach (var us in users)
+            {
+                sync.InsertSync(us,colUserName);
+            }
+            
+            sync.UpdateSync(users[0], colUserName);
+
+            sync.DeleteSync(users[3], colUserName);
+
+            var changes = sync.PullChangesResult<User>(0, colUserName, DateTime.UtcNow);
+
+            var expectedUsersCreated = 2;
+            var expectedUsersUpdated = 1;
+            var expectedUsersDeleted = 1;
+
+            var totalUsersCreated = changes.Changes.Created.Count();
+            var totalUsersUpdated = changes.Changes.Updated.Count();
+            var totalUsersDeleted = changes.Changes.Deleted.Count();
+
+
+            Assert.Equal(expectedUsersCreated, totalUsersCreated);
+            Assert.Equal(expectedUsersUpdated, totalUsersUpdated);
+            Assert.Equal(expectedUsersDeleted, totalUsersDeleted);
+        }
 
         public class User(Guid id) : BaseSync(id)
         {
