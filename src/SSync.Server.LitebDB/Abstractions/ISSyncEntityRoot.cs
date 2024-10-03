1bb54690c18fdc92f4f6f033804a36d4b24daf87
Gabriel Sales <ghe_sales@hotmail.com>
2024-10-02 23:43:27 -0300

    To change interface to abstract class entity root server to create entities

--- src/SSync.Server.LitebDB/Abstractions/ISSyncEntityRoot.cs
+++ src/SSync.Server.LitebDB/Abstractions/ISSyncEntityRoot.cs
@@ -1,9 +1,26 @@
-﻿namespace SSync.Server.LitebDB.Abstractions
+﻿using SSync.Server.LitebDB.Enums;
+
+namespace SSync.Server.LitebDB.Abstractions
 {
-    public interface ISSyncEntityRoot
+    public abstract class ISSyncEntityRoot
     {
+        protected ISSyncEntityRoot(Time time)
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = time == Time.LOCAL_TIME ? DateTime.Now : DateTime.UtcNow;
+        }
+        protected ISSyncEntityRoot(Guid id, Time time)
+        {
+            Id = id;
+            CreatedAt = time == Time.LOCAL_TIME ? DateTime.Now : DateTime.UtcNow;
+        }
         public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
+
+        void SetUpdatedAt(DateTime dateTime) => UpdatedAt = dateTime;
+        void SetDeletedAt(DateTime dateTime) => DeletedAt = dateTime;
+
     }
 }
