5e980e4ebee1820dbb508d76d322cb09b05baa94
Gabriel Sales <ghe_sales@hotmail.com>
2025-02-26 21:40:01 -0300

    Remove comments from interface

--- src/SSync.Client.SQLite/Abstractions/Sync/ISynchronize.cs
+++ src/SSync.Client.SQLite/Abstractions/Sync/ISynchronize.cs
@@ -1,33 +1,10 @@
 ﻿
-using SQLite;
 using SSync.Client.SQLite.Poco;
 
 namespace SSync.Client.SQLite.Abstractions.Sync
 {
     public interface ISynchronize
     {
-        //DateTime GetLastPulledAt();
-        //BsonValue ReplaceLastPulledAt(DateTime lastPulledAt);
-
-        //BsonValue DeleteSync<T>(T entity, ILiteCollection<T> col) where T : SchemaSync;
-
-        //BsonValue DeleteSync<T>(T entity, string? colName) where T : SchemaSync;
-
-        //void DumpLogOutput(string title = "log.txt");
-
-        //T FindByIdSync<T>(Guid id, ILiteCollection<T> col) where T : SchemaSync;
-
-        //T FindByIdSync<T>(Guid id, string? colName) where T : SchemaSync;
-        //BsonValue InsertSync<T>(T entity, ILiteCollection<T> col) where T : SchemaSync;
-
-        //BsonValue InsertSync<T>(T entity, string? colName) where T : SchemaSync;
-
-        //SchemaPullResult<T> PullChangesResult<T>(DateTime lastPulledAt, string collectionName) where T : SchemaSync;
-
-        //SchemaPush<T> PushChangesResult<T>(SchemaPush<T> schemaPush) where T : SchemaSync;
-        //BsonValue UpdateSync<T>(T entity, string? colName) where T : SchemaSync;
-
-        //BsonValue UpdateSync<T>(T entity, ILiteCollection<T> col) where T : SchemaSync;
         Task<int> DeleteSyncAsync<T>(T entity) where T : SchemaSync, new();
         void Dispose();
         void DumpLogOutput(string title = "log.txt");
